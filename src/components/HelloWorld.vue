f7b1f6048807f9315b65bed59730c426bd1556d9
elnoubi <30166630+Elnoubi8@users.noreply.github.com>
2022-01-10 20:53:41 +0200

    firtst ciommit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,109 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-container>
+    <v-file-input
+      accept="image/png, image/jpeg, image/bmp"
+      placeholder="Pick an avatar"
+      prepend-icon="mdi-camera"
+      label="Avatar"
+      v-model="imagge"
+      @change="postForm()"
+    ></v-file-input>
+    <v-row>
+      <v-col cols="6">
+        <v-container>
+          <v-img height="300" :src="url"></v-img>
+        </v-container>
+      </v-col>
+      <v-col cols="6">
+        <plotly
+          v-if="plot"
+          :data="data"
+          :layout="layout"
+          :displayModeBar="false"
+        />
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="6">
+        <v-container> 
+                 <plotly
+          v-if="plot"
+          :data="data"
+          :layout="layout"
+          :displayModeBar="false"
+        />
+        </v-container>
+      </v-col>
+      <v-col cols="6">
+        <v-col class="d-flex" cols="12" sm="6">
+          <v-select  v-if="plot" v-model="selected" :items="itemss" @change="changeSelect()" label="Select type"></v-select>
+        </v-col>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
+import { Plotly } from "vue-plotly";
+import axios from "axios";
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  name: "HelloWorld",
+  components: {
+    Plotly,
+  },
+  data: () => ({
+    plot: false,
+    items: ['background', 'cap','ring', 'stipe', 'gills', 'volva', 'mycelium'], 
+    selected: 0,
+    data: [
+      {
+        z: [],
+        type: "contour",
+        autorange: "reversed",
+      },
+    ],
+    layout: {
+      title: "My graph",
+      yaxis: { autorange: "reversed" },
+    },
+    imagge: null,
+    url: null,
+  }),
+  computed: {
+    itemss() {
+      return this.items.map((item, index) => {
+        return {
+          text: item,
+          value: index
+        }
+      })
+    }
+  },
+  methods: {
+    postForm() {
+      console.log("hhere", this.imagge);
+      this.url = URL.createObjectURL(this.imagge);
+      let formData = new FormData();
+      formData.append("image", this.imagge);
+      axios
+        .post("http://localhost:105/form-example", formData)
+        .then((response) => {
+          this.data[0].z = response.data;
+          console.log(this.data);
+          this.plot = true;
+        });
+    },
+    changeSelect() {
+       this.url = URL.createObjectURL(this.imagge);
+      let formData = new FormData();
+      formData.append("image", this.imagge);
+      formData.append("item", this.selected);
+      axios
+        .post("http://localhost:105/change", formData)
+        .then(() => {
+          this.plot = true;
+        });
+    },
+  },
+};
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
